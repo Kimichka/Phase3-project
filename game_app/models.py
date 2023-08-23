b01252d6fce7d2b44a53246fb81eeae5fe8d4e76
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-08-23 14:42:32 -0400

    Creates two classes

--- game_app/models.py
+++ game_app/models.py
@@ -8,3 +8,17 @@ game_platform_association = Table('game_platform', Base.metadata,
     Column('game_id', Integer, ForeignKey('games.id')),
     Column('platform_id', Integer, ForeignKey('platforms.id'))
 )
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, unique=True, nullable=False)
+    platforms = relationship('Platform', secondary=game_platform_association, back_populates='games')
+
+class Platform(Base):
+    __tablename__ = 'platforms'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    games = relationship('Game', secondary=game_platform_association, back_populates='platforms')
