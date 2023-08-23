6887e29db2f25ef46d91496752e28635a9e42905
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-08-23 14:46:16 -0400

    Adds database on cli

--- game_app/cli.py
+++ game_app/cli.py
@@ -2,3 +2,8 @@ import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base, Game, Platform, game_platform_association
+
+DATABASE_URL = "sqlite:///./games.db"
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
