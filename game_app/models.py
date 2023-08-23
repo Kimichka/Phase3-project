263a46c26f24baa18d291f5da34a2390ad886736
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-08-23 14:41:35 -0400

    Updates game platform on models

--- game_app/models.py
+++ game_app/models.py
@@ -3,3 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+game_platform_association = Table('game_platform', Base.metadata,
+    Column('game_id', Integer, ForeignKey('games.id')),
+    Column('platform_id', Integer, ForeignKey('platforms.id'))
+)
