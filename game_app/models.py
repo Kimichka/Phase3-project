6f27d3c35be91a1133355ecdd0ff00d95a233bbd
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-08-23 14:40:54 -0400

    updates models

---
+++ game_app/models.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
